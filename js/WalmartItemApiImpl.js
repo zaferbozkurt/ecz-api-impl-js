ebe3065e55571cd64ffc2ec89f81b407ebfd3986
Zafer Bozkurt <zbzkurt@gmail.com>
2022-01-12 13:45:19 +0300

    api generatipn

--- js/WalmartItemApiImpl.js
+++ js/WalmartItemApiImpl.js
@@ -8,3 +8,15 @@ WalmartItemApi.requests().registerAddItemRequestHandler((requestData) => {
     //TODO: Write your implementation and resolve or reject the promise
     //return Promise.resolve(result) || Promise.reject(err)
 })
+WalmartItemApi.requests().registerGetUsersRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
+WalmartItemApi.requests().registerAddUserRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
+WalmartItemApi.requests().registerClickDetailRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
