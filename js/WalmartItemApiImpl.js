db35b4e5e2bc78f021f5930335e25b4801b481bc
Zafer Bozkurt <zbzkurt@gmail.com>
2022-01-12 09:22:09 +0300

    initial commit

---
+++ js/WalmartItemApiImpl.js
@@ -0,0 +1,10 @@
+import { WalmartItemApi } from 'ecz-api'
+
+WalmartItemApi.requests().registerFindItemsRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
+WalmartItemApi.requests().registerAddItemRequestHandler((requestData) => {
+    //TODO: Write your implementation and resolve or reject the promise
+    //return Promise.resolve(result) || Promise.reject(err)
+})
